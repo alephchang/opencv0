58847628e459de4e6c13c1b641b39734c0f076fc
alephchang <alephchang@foxmail.com>
2017-12-12 18:01:08 +0800

    Rectify image interactively

--- homography.cpp
+++ homography.cpp
@@ -1,93 +1,112 @@
-/*
-* @file SURF_FlannMatcher
-* @brief SURF detector + descriptor + FLANN Matcher
-* @author A. Huaman
-*/
-#if 0
-#include <stdio.h>
+#include "stdafx.h"
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+#include <opencv2/opencv.hpp>
 #include <iostream>
-#include <stdio.h>
-#include <iostream>
-#include "opencv2/core.hpp"
-#include "opencv2/features2d.hpp"
-#include "opencv2/imgcodecs.hpp"
-#include "opencv2/highgui.hpp"
-#include "opencv2/xfeatures2d.hpp"
+
 using namespace std;
 using namespace cv;
-using namespace cv::xfeatures2d;
-void readme();
-/*
-* @function main
-* @brief Main function
-*/
-int main(int argc, char** argv)
+const int Quad = 4;
+static vector<Point2i> sQuadPoints;
+static Point2i sCurrentPt;
+
+void CallBackFunc(int event, int x, int y, int flags, void* userdata)
 {
-	if (argc != 3)
+	if (event == EVENT_LBUTTONDOWN)
 	{
-		readme(); return -1;
-	}
-	const char* p0 = "F:\\opencv\\sources\\samples\\data\\box.jpg";
-	const char* p1 = "F:\\opencv\\sources\\samples\\data\\box_in_scene.jpg";
-	Mat img_1 = imread(p0, IMREAD_GRAYSCALE);
-	Mat img_2 = imread(p1, IMREAD_GRAYSCALE);
-	if (!img_1.data || !img_2.data)
-	{
-		std::cout << " --(!) Error reading images " << std::endl; return -1;
-	}
-	//-- Step 1: Detect the keypoints using SURF Detector, compute the descriptors
-	int minHessian = 400;
-	Ptr<SURF> detector = SURF::create();
-	detector->setHessianThreshold(minHessian);
-	std::vector<KeyPoint> keypoints_1, keypoints_2;
-	Mat descriptors_1, descriptors_2;
-	detector->detectAndCompute(img_1, Mat(), keypoints_1, descriptors_1);
-	detector->detectAndCompute(img_2, Mat(), keypoints_2, descriptors_2);
-	//-- Step 2: Matching descriptor vectors using FLANN matcher
-	FlannBasedMatcher matcher;
-	std::vector< DMatch > matches;
-	matcher.match(descriptors_1, descriptors_2, matches);
-	double max_dist = 0; double min_dist = 100;
-	//-- Quick calculation of max and min distances between keypoints
-	for (int i = 0; i < descriptors_1.rows; i++)
-	{
-		double dist = matches[i].distance;
-		if (dist < min_dist) min_dist = dist;
-		if (dist > max_dist) max_dist = dist;
-	}
-	printf("-- Max dist : %f \n", max_dist);
-	printf("-- Min dist : %f \n", min_dist);
-	//-- Draw only "good" matches (i.e. whose distance is less than 2*min_dist,
-	//-- or a small arbitary value ( 0.02 ) in the event that min_dist is very
-	//-- small)
-	//-- PS.- radiusMatch can also be used here.
-	std::vector< DMatch > good_matches;
-	for (int i = 0; i < descriptors_1.rows; i++)
-	{
-		if (matches[i].distance <= max(2 * min_dist, 0.02))
-		{
-			good_matches.push_back(matches[i]);
+		cout << "Left button of the mouse is clicked - position (" << x << ", " << y << ")" << endl;
+		if (sQuadPoints.size() == Quad)
+			cout << "Cannot choose more point!" << std::endl;
+		else {
+			sQuadPoints.push_back(Point2i(x, y));
 		}
 	}
-	//-- Draw only "good" matches
-	Mat img_matches;
-	drawMatches(img_1, keypoints_1, img_2, keypoints_2,
-		good_matches, img_matches, Scalar::all(-1), Scalar::all(-1),
-		vector<char>(), DrawMatchesFlags::NOT_DRAW_SINGLE_POINTS);
-	//-- Show detected matches
-	imshow("Good Matches", img_matches);
-	for (int i = 0; i < (int)good_matches.size(); i++)
+	else if (event == EVENT_RBUTTONDOWN)
 	{
-		printf("-- Good Match [%d] Keypoint 1: %d  -- Keypoint 2: %d  \n", i, good_matches[i].queryIdx, good_matches[i].trainIdx);
+		cout << "Right button of the mouse is clicked - position (" << x << ", " << y << ")" << endl;
+		if (sQuadPoints.empty())
+			cout << "Cannot remove more point!" << endl;
+		else
+			sQuadPoints.pop_back();
 	}
-	waitKey(0);
+	else if (event == EVENT_MOUSEMOVE)
+	{
+		sCurrentPt = Point2i(x, y);
+	}
+}
+void help()
+{
+	cout << "This binary is used to rectify image." << endl;
+	cout << "Step 1. Move mouse and click left button to select four points for the target in order." << endl;
+	cout << "Step 2. Click right button to cancel one point selection." << endl;
+	cout << "Setp 3. After selecting four points, press ENTER to show the rectification result." << endl;
+}
+int main(int argc, char** argv)
+{
+	// Read image from file 
+	if (argc < 2) {
+		cout << "Please specify input image path" << endl;
+		return 0;
+	}
+	help();
+
+	Mat img = imread(argv[1]);
+
+	//if fail to read the image
+	if (img.empty())
+	{
+		cout << "Error loading the image: "<<argv[1] << endl;
+		return -1;
+	}
+	
+	//Create a window
+	namedWindow("Window", 1);
+	Mat imgShown;
+
+	bool showResult = false;
+	while (1) {
+		if (!showResult) {
+			img.copyTo(imgShown);
+			//set the callback function for any mouse event
+			setMouseCallback("Window", CallBackFunc, NULL);
+
+			if (sQuadPoints.size() >0 && sQuadPoints.size() < 4) {
+				line(imgShown, sCurrentPt, sQuadPoints[0], Scalar(255, 0, 0), 1, 4);
+				line(imgShown, sCurrentPt, sQuadPoints.back(), Scalar(255, 0, 0), 1, 4);
+
+				polylines(imgShown, sQuadPoints, false, Scalar(250, 255, 0), 1);
+			}
+			else if(sQuadPoints.size()==4)
+				polylines(imgShown, sQuadPoints, true, Scalar(250, 250, 0), 1);
+		}
+		//show the image
+		
+		imshow("Window", imgShown);
+
+
+		// Wait until user press some key
+		int key = waitKey(10);
+		if (key == 27)
+			break;
+		else if (key == 13) {
+			if (sQuadPoints.size() == Quad) {
+				//Get bound box
+				Rect2i rect = boundingRect(sQuadPoints);
+				vector<Point2i> dstPts(4);
+				dstPts[0] = Point2i(0, 0);
+				dstPts[1] = Point2i(0, rect.height);
+				dstPts[2] = Point2i(rect.width, rect.height);
+				dstPts[3] = Point2i(rect.width, 0);
+				//Do homograph
+				Mat H = findHomography(sQuadPoints, dstPts);
+				std::cout << "Homography Matrix is\n"<< H << std::endl;
+				//store the result in @imgShown
+				imgShown.create(rect.height,rect.width, img.type());
+				warpPerspective(img, imgShown, H, imgShown.size());
+				showResult = true;
+			}
+		}
+	}
+
 	return 0;
 }
-/*
-* @function readme
-*/
-void readme()
-{
-	std::cout << " Usage: ./SURF_FlannMatcher <img1> <img2>" << std::endl;
-}
-#endif
